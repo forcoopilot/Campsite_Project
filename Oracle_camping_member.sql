89c0a4b0bcb7852888aeef2a8c983df5a1d32ef0
607호 <607@DESKTOP-L6KE6U2>
2022-09-19 10:41:11 +0900

    1

--- Oracle_camping_member.sql
+++ Oracle_camping_member.sql
@@ -1,4 +1,14 @@
-
+create table camping_member(
+	id varchar2(20) primary key,
+	passwd1 varchar2(20) not null,
+	name varchar2(30) not null,
+	email varchar2(30) not null,
+	tele varchar2(20) not null,
+	hobby varchar2(30),
+	job varchar2(20),
+	age varchar2(10),
+	info varchar2(3000)
+	);
 
 commit;
 
